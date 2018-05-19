95b59b5ff92f5f1715df8b1b32dda18100138da2
krzysiu26911 <227086@student.pwr.edu.pl>
2018-05-19 15:33:24 +0200

    starter

---
+++ react_starter-master/src/views/Home/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import MainContainer from '../../ui/mainContainer';
+import App from '../../components/app';
+
+export default () => (
+  <MainContainer>
+    <App />
+  </MainContainer>
+);
